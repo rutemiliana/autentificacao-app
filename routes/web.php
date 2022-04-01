6fae533f15028d87d5c39ad6e2833f80876e1aa1
Rute Miliana <rutemiliana13@gmail.com>
2022-04-01 12:36:22 -0300

    enviando emails, alterando view de mensagem teste e disparando email na web route

--- routes/web.php
+++ routes/web.php
@@ -34,5 +34,10 @@ Route::resource('tarefa', TarefaController::class);
 //ver TarefaController o método  __construct()
 
 Route::get('mensagem-teste' , function (){
-    return new MensagemTesteMail();
+    //return new MensagemTesteMail();
+
+    // o comando abaixo pode ser testado pelo tinker usando:
+    // use App\Mail\MensagemTesteMail;
+    Mail::to('email')->send(new MensagemTesteMail());
+    return 'E-mail enviado com sucesso!';
 });
